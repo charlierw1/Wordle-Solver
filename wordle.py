3534bc3df7f0d426f598131ec045a500adc5f4ed
charlierw1 <95688671+charlierw1@users.noreply.github.com>
2026-05-25 01:54:56 +0100

    Made basic frame for program
    Added the majority of the basic frame of the program, but several issues remain

---
+++ wordle.py
@@ -0,0 +1,102 @@
+
+# Available letter variables
+first_slot_letters = []
+second_slot_letters = []
+third_slot_letters = []
+fourth_slot_letters = []
+fifth_slot_letters = []
+
+# Command List
+cmds = '''
+help - List all commands
+exit - Exit the program
+'''
+
+while True:
+    command = input("Enter a command: ")
+    match command.lower():
+        case "help":
+            print(cmds)
+        case "letters":
+            first_slot_letters.clear()
+            while True:
+                letter = input("Enter the first letter (type done when all possible answers have been entered): ")
+                if letter.lower() == "done":
+                    break
+                else:
+                    if len(letter) == 1 and letter.isalpha() and letter not in first_slot_letters:
+                        first_slot_letters.append(letter)
+                    else:
+                        print("That is not a valid letter or it is already in the list!")
+                print(first_slot_letters)
+            second_slot_letters.clear()
+            while True:
+                letter = input("Enter the second letter (type done when all possible answers have been entered): ")
+                if letter.lower() == "done":
+                    break
+                else:
+                    if len(letter) == 1 and letter.isalpha() and letter not in second_slot_letters:
+                        second_slot_letters.append(letter)
+                    else:
+                        print("That is not a valid letter or it is already in the list!")
+                print(second_slot_letters)
+            third_slot_letters.clear()
+            while True:
+                letter = input("Enter the third letter (type done when all possible answers have been entered): ")
+                if letter.lower() == "done":
+                    break
+                else:
+                    if len(letter) == 1 and letter.isalpha() and letter not in third_slot_letters:
+                        third_slot_letters.append(letter)
+                    else:
+                        print("That is not a valid letter or it is already in the list!")
+                print(third_slot_letters)
+            fourth_slot_letters.clear()
+            while True:
+                letter = input("Enter the fourth letter (type done when all possible answers have been entered): ")
+                if letter.lower() == "done":
+                    break
+                else:
+                    if len(letter) == 1 and letter.isalpha() and letter not in fourth_slot_letters:
+                        fourth_slot_letters.append(letter)
+                    else:
+                        print("That is not a valid letter or it is already in the list!")
+                print(fourth_slot_letters)
+            fifth_slot_letters.clear()
+            while True:
+                letter = input("Enter the fifth letter (type done when all possible answers have been entered): ")
+                if letter.lower() == "done":
+                    break
+                else:
+                    if len(letter) == 1 and letter.isalpha() and letter not in fifth_slot_letters:
+                        fifth_slot_letters.append(letter)
+                    else:
+                        print("That is not a valid letter or it is already in the list!")
+                print(fifth_slot_letters)
+        case "solve":
+            generated_words = []
+            for l1 in first_slot_letters:
+                for l2 in second_slot_letters:
+                    for l3 in third_slot_letters:
+                        for l4 in fourth_slot_letters:
+                            for l5 in fifth_slot_letters:
+                                word = f"{l1}{l2}{l3}{l4}{l5}"
+                                print(word)
+                                generated_words.append(word)
+
+            accepted_words = []
+
+            with open("words.txt") as f:
+                words = f.read().lower()
+
+            for word in generated_words:
+                if word in words:
+                    accepted_words.append(word)
+            
+            print(accepted_words)
+
+                
+        case "exit":
+            exit()
+        case unknown_command:
+            print("Command not recognised, try 'help'")
